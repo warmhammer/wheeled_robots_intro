73ff71233351f6e72530aa158d0c58eba16f4b3a
Maxim Popov <max.popov.02@ya.ru>
2024-07-31 23:47:42 +0300

    lec 5 update: docker installation + build, devel dirs were deleted

--- lec_5_ros/catkin_ws/build/talker/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /catkin_ws/src/talker
-# Build directory: /catkin_ws/build/talker
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- lec_5_ros/install_docker.sh
+++ lec_5_ros/install_docker.sh
@@ -16,17 +16,4 @@ sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
-
-# NVIDIA Container Toolkit
-if [[ $1 = "--nvidia" ]] || [[ $1 = "-n" ]]
-  then
-      distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-      curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-      curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-
-      sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
-      sudo systemctl restart docker
-fi
-
-
 newgrp docker
